6827095c6c27b379ddcfb54f2ca309dbb27f14bb
Tamir <1tamir198@gmail.com>
2021-09-23 01:26:37 +0300

    Upload finished project

--- Networking/IpGeolocation.js
+++ Networking/IpGeolocation.js
@@ -1,19 +1,18 @@
 
-const API_KEY = "KEY";
+const API_KEY = "API_KEY";
 
 export default class IpGeolocation {
-
-  location
+  locationData;
 
   async getAddress(ip) {
-    const url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`
+    const url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`;
 
    await fetch(url)
       .then(res => res.json())
-      .then(data => this.setLocation(data))
+      .then(data => this.setLocation(data));
   }
   
   setLocation(data){
-    this.location = data;
+    this.locationData = data;
   }
 }
