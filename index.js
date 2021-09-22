0c18a37b18424035bd6e7e071c45df4e3dbfe500
Tamir <1tamir198@gmail.com>
2021-09-22 23:49:50 +0300

    Map is now focused on current location when entering the page

--- index.js
+++ index.js
@@ -2,34 +2,41 @@ import CurrentLocation from "./Location/CurrentLocation.js";
 import MapView from "./Map/map.js";
 import IpGeolocation from "./Networking/IpGeolocation.js";
 window.onload = () => {
+
   const geoLocation = new IpGeolocation();
   const mapView = new MapView();
 
   mapView.initMap();
-  setMapToCurrentLocation(mapView);
 
+  // updateLocationUI(geoLocation);
+  // setMapToCurrentLocation(mapView);
 
   document.querySelector(".submit-ip-btn").addEventListener("click", () => {
-    const ip = document.querySelector(".ip-input").value;
-    geoLocation.getAddress(ip).then(() => setDetailsForLocation(geoLocation.location,ip));
-
-  })};
+    updateLocationUI(geoLocation,mapView);
+  });
+};
 
 const setMapToCurrentLocation = (mapView)=>{
-
   const currentLocation = new CurrentLocation();
 
   currentLocation.getCurrentLocation().then(() =>
   mapView.setMapLocation(currentLocation.currentLatitude, currentLocation.currentLongitude));
 }
 
+const updateLocationUI = (geoLocation,mapView)=>{
+  const ip = document.querySelector(".ip-input").value;
 
-const setDetailsForLocation = (data,ip) =>{
-  console.log(data);
+  geoLocation.getAddress(ip).then(() => {
+    changeLocationUI(geoLocation.locationData);
+    mapView.setMapLocation(geoLocation.locationData.location.lat,geoLocation.locationData.location.lng)
+  });
+}
+
+const changeLocationUI = (data) =>{
   document.getElementById("location").innerHTML  = `${data.location.country}-${data.location.region} <br> ${data.location.city}`;
   document.getElementById("timezone").innerHTML  = `UTC ${data.location.timezone}`;
-  document.getElementById("ip").innerHTML  = ip;
+  document.getElementById("ip").innerHTML  = data.ip;
   document.getElementById("coordinates").innerHTML  = `${data.isp}`;
-  
-
 }
+
+
